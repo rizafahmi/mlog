39095cf9f329fb5683a84ce80a10f60dc83d5c8d
Riza Fahmi <rizafahmi@gmail.com>
2014-03-13 17:46:32 +0700

    Add postItem

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -2,7 +2,7 @@ var postsData = [
   {
     title: 'First post',
     datePublish: new Date(),
-    content: '<p>Hello, this is our very first post!'
+    content: '<p>Hello, this is our very first post!. As a starter, as usual, let me say: <h1>Hello nurse!</h1><p>'
   }
 ];
 
