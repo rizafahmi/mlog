5c31dbf93f36eca7543e03d044fcc0f1569cfc74
Riza Fahmi <rizafahmi@gmail.com>
2014-03-15 09:52:39 +0700

    List data from Posts collection

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,16 +1,5 @@
-var postsData = [
-  {
-    title: 'First post',
-    datePublish: new Date(),
-    content: '<p>Hello, this is our very first post!. As a starter, as usual, let me say: <h1>Hello nurse!</h1><p>'
-  },
-  {
-    title: 'Second Post is...',
-    datePublish: new Date(),
-    content: '<p>You know lah....</p>'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+           return Posts.find();
+         }
 });
