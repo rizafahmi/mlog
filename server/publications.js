2a33ec9e7d693e3687fe655f7ada990b212ee9bc
Riza Fahmi <rizafahmi@gmail.com>
2014-03-18 16:35:13 +0700

    Made simple pub/sub for comments depend on postId

--- server/publications.js
+++ server/publications.js
@@ -6,6 +6,6 @@ Meteor.publish('users', function(){
   return Meteor.users.find();
 });
 
-Meteor.publish('comments', function(){
-  return Comments.find();
+Meteor.publish('comments', function(postId){
+  return Comments.find({postId: postId});
 });
